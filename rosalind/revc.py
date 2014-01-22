489443ef2d841dd6ed1611abda47e404491cbb91
Smolina Irina <smolirinka@gmail.com>
2014-01-22 10:14:26 +0600

    solved revc

---
+++ rosalind/revc.py
@@ -0,0 +1,18 @@
+# Problem http://rosalind.info/problems/revc/
+# The Secondary and Tertiary Structures of DNA
+
+# In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.
+# The reverse complement of a DNA string s is the string sc formed by reversing the symbols of s,
+# then taking the complement of each symbol (e.g., the reverse complement of "GTCA" is "TGAC").
+
+# Given: A DNA string s of length at most 1000 bp.
+# Return: The reverse complement sc of s.
+
+import sys
+
+complements = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
+for filename in sys.argv[1:]:
+    for line in open(filename):
+        s = [complements[ch] for ch in line.strip()] 
+        s = "".join(s) #complement string
+        print(s[::-1])   #reverse string    
